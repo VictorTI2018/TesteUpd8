4d8d4cfca4f6df23140bc2266385659e91b4410a
VictorTI2018 <victorhugoTI@outlook.com>
2022-10-26 14:20:47 -0300

    #Criando Service Base

---
+++ Upd8/Upd8.Core.Domain/Interfaces/Services/IServiceBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Upd8.Core.Domain.Entities;
+
+namespace Upd8.Core.Domain.Interfaces.Services
+{
+    public interface IServiceBase<TEntity, TKey> where TEntity : BaseEntity
+    {
+        Task<TEntity> CreateAsync(TEntity entity);
+
+        Task<IEnumerable<TEntity>> GetAsync();
+
+        Task<TEntity> GetByIdAsync(TKey id);
+
+        Task<TEntity> UpdateAsync(TEntity entity);
+
+        Task<bool> DeleteAsync(TKey id);
+    }
+}
---
+++ Upd8/Upd8.Services/ServiceBase.cs
@@ -0,0 +1,41 @@
+﻿using Upd8.Core.Domain.Entities;
+using Upd8.Core.Domain.Interfaces.Repositories;
+using Upd8.Core.Domain.Interfaces.Services;
+
+namespace Upd8.Services
+{
+    public class ServiceBase<TEntity, TKey> : IServiceBase<TEntity, TKey> where TEntity : BaseEntity
+    {
+        protected readonly IRepositoryBase<TEntity, TKey> _repoBase;
+
+        public ServiceBase(IRepositoryBase<TEntity, TKey> repositoryBase)
+        {
+            _repoBase = repositoryBase;
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAsync()
+        {
+            return await _repoBase.GetAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(TKey id)
+        {
+            return await _repoBase.GetByIdAsync(id);
+        }
+
+        public async Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            return await _repoBase.UpdateAsync(entity);
+        }
+
+        public async Task<TEntity> CreateAsync(TEntity entity)
+        {
+            return await _repoBase.CreateAsync(entity);
+        }
+
+        public async Task<bool> DeleteAsync(TKey id)
+        {
+            return await _repoBase.DeleteAsync(id);
+        }
+    }
+}
