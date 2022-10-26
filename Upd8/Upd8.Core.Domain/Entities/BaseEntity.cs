25b155ed6faa946c1d2a26cfcbc64e9af961b657
VictorTI2018 <victorhugoTI@outlook.com>
2022-10-26 12:45:57 -0300

    #Criando Entidade Base

--- Upd8/Upd8.Core.Domain/Entities/BaseEntity.cs
+++ Upd8/Upd8.Core.Domain/Entities/BaseEntity.cs
@@ -2,6 +2,21 @@
 {
     public class BaseEntity
     {
-        public int Id { get; set; }
+        public BaseEntity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+
+        private DateTime? _createdAt;
+
+        public DateTime? CreatedAt
+        {
+            get { return _createdAt; }
+            set { _createdAt = value == null ? DateTime.UtcNow : value; }
+        }
+
+        public DateTime? UpdateAt { get; set; }
     }
 }
